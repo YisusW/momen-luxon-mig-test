e2131aea6982b2efe6f639028e68adcbe8a4c36a
YisusW <jesusochoa266@gmail.com>
2022-09-21 21:08:14 -0500

    moment and luxon format and toString equivalent

--- index.ts
+++ index.ts
@@ -2,12 +2,7 @@ import { DateTime } from 'luxon';
 import moment from 'moment';
 
 console.log(
-  `moment diff => ${moment().diff(
-    moment().utc().subtract(100, 'hours'),
-    'hours'
-  )}
-   luxonD diff => ${
-     DateTime.local().diff(DateTime.utc().minus({ hours: 100 }), 'hours').hours
-   }
+  `moment format => ${moment().format()}
+   luxonD format => ${DateTime.now().toString()}
   `
 );
