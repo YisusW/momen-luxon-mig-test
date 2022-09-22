e1cf380a088eddcdfb10c6b7855d83fcc23eab51
YisusW <jesusochoa266@gmail.com>
2022-09-21 21:25:25 -0500

    to format `HH:mm` luxon and moment

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 import moment from 'moment';
 
 console.log(
-  `moment format => ${moment().format()}
-   luxonD format => ${DateTime.now().toString()}
+  `moment format => ${moment().format('HH:mm')}
+   luxonD format => ${DateTime.now().toFormat('HH:mm')}
   `
 );
