36b9bd09fbaa3ef4bb70e88be38d367364aa974a
YisusW <jesusochoa266@gmail.com>
2022-09-27 10:47:29 -0500

    get year from now

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 import moment from 'moment';
 
 console.log(
-  `moment format => ${moment().format('DD-MMM HH:mm')}
-   luxonD format => ${DateTime.now().toFormat('d-MMM HH:mm')}
+  `moment format => ${moment().year()}
+   luxonD format => ${DateTime.now().year}
   `
 );
