0360eb6bfddc896c7d65d3455e6548523bacdd4f
YisusW <jesusochoa266@gmail.com>
2022-09-27 19:28:58 -0500

    luxon equal or is after of other DateTime

--- index.ts
+++ index.ts
@@ -1,8 +1,47 @@
-import { DateTime } from 'luxon';
+import { DateTime, Interval } from 'luxon';
 import moment from 'moment';
 
+const date = new Date(2016, 5, 5);
+
+// after
+
 console.log(
-  `moment format => ${moment().year()}
-   luxonD format => ${DateTime.now().year}
+  `moment should be after (true) => ${moment().isSameOrAfter(date)}
+   luxonD should be after (true) => ${Interval.before(
+     DateTime.now(),
+     0
+   ).isAfter(DateTime.fromJSDate(date))}
+  `
+);
+
+// before
+
+console.log(
+  `moment should be false => ${moment('2015-10-20').isSameOrAfter('2016-5-1')}
+   luxona should be false => ${Interval.before(
+     DateTime.fromJSDate(new Date(2015, 10, 20)),
+     0
+   ).isAfter(DateTime.fromJSDate(new Date(2016, 5, 1)))}
+  `
+);
+
+// equals
+
+console.log(
+  `moment should be true => ${moment('2015-10-20').isSameOrAfter('2016-5-1')}
+   luxona should be true => ${Interval.before(
+     DateTime.fromJSDate(new Date(2015, 10, 20)),
+     0
+   ).isAfter(DateTime.fromJSDate(new Date(2016, 5, 1)))}
+  `
+);
+
+const sameDate = DateTime.fromJSDate(new Date(2015, 10, 20));
+
+console.log(
+  `moment should be false => ${moment('2015-10-20').isSameOrAfter('2015-10-20')}
+   luxona should be false => ${Interval.before(sameDate, 0).equals(
+     Interval.after(sameDate, 0)
+   )}
   `
 );
