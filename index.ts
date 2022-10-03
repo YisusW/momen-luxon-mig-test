5efaca86adddec6301b94679dd8f4875151649dd
YisusW <jesusochoa266@gmail.com>
2022-10-03 15:50:38 -0500

    mig(format): moment to luxon to format 'DD-MMM HH:mm'

--- index.ts
+++ index.ts
@@ -3,45 +3,15 @@ import moment from 'moment';
 
 const date = new Date(2016, 5, 5);
 
-// after
-
 console.log(
-  `moment should be after (true) => ${moment().isSameOrAfter(date)}
-   luxonD should be after (true) => ${Interval.before(
-     DateTime.now(),
-     0
-   ).isAfter(DateTime.fromJSDate(date))}
-  `
-);
-
-// before
-
-console.log(
-  `moment should be false => ${moment('2015-10-20').isSameOrAfter('2016-5-1')}
-   luxona should be false => ${Interval.before(
-     DateTime.fromJSDate(new Date(2015, 10, 20)),
-     0
-   ).isAfter(DateTime.fromJSDate(new Date(2016, 5, 1)))}
-  `
-);
-
-// equals
-
-console.log(
-  `moment should be true => ${moment('2015-10-20').isSameOrAfter('2016-5-1')}
-   luxona should be true => ${Interval.before(
-     DateTime.fromJSDate(new Date(2015, 10, 20)),
-     0
-   ).isAfter(DateTime.fromJSDate(new Date(2016, 5, 1)))}
-  `
-);
-
-const sameDate = DateTime.fromJSDate(new Date(2015, 10, 20));
-
-console.log(
-  `moment should be false => ${moment('2015-10-20').isSameOrAfter('2015-10-20')}
-   luxona should be false => ${Interval.before(sameDate, 0).equals(
-     Interval.after(sameDate, 0)
+  `moment should be after => ${moment().isSameOrAfter(date)}
+   luxonD should be after => ${Interval.before(DateTime.now(), 0).isAfter(
+     DateTime.fromJSDate(date)
    )}
   `
+);
+
+console.log(
+  `moment format => ${moment().format('DD-MMM HH:mm')}
+  luxonD format => ${DateTime.now().toFormat('dd-MMM HH:mm')}`
 );
